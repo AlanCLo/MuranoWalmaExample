ca1a9ca3284bccec9bbf1dc6c36c613bd03be58b
Alan Lo <lo.alan@gmail.com>
2016-09-27 13:21:14 +1000

    Made it work to nectar cloud with only 14.04. Walma has problems with newer version of nodejs

--- Resources/scripts/deploy.sh
+++ Resources/scripts/deploy.sh
@@ -1,26 +1,33 @@
 #!/bin/bash -v
 
-## Install package dependencies
+## Install MongoDB Ubuntu 14.04
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
-## Upgrade would be nice but it adds a lot to the startup time to this application
-#sudo apt-get upgrade -y
-sudo apt-get install gzip git curl python libssl-dev pkg-config build-essential nodejs-legacy npm supervisor -y
+sudo apt-get install -y mongodb-org
+
+
+## Install package dependencies
+sudo apt-get install git nodejs-legacy npm supervisor -y
 
 ## Fetch software, configure and install
 cd /opt
-git clone --branch 1.6.0 https://github.com/ether/etherpad-lite.git etherpad-lite
-cd etherpad-lite
-sed -i 's/"port" : 9001/"port" : 80/' settings.json.template
-bin/installDeps.sh > log.txt 2> error.txt
+git clone git://github.com/opinsys/walma.git
+cd walma
+git checkout f6fb11d47feaa1597cfd1aacbf8d09aeaff3f769
+npm install
+bin/setupdb
+sed -i 's/"listenPort": 1337/"listenPort": 80/' config.json
 
-## Setup supervisord to keep the service running
-cat << EOT >> /etc/supervisor/conf.d/etherpad.conf
-[program:etherpad]
-command=/opt/etherpad-lite/bin/run.sh --root
+# Setup supervisord to keep the service running
+cat << EOT >> /etc/supervisor/conf.d/walma.conf
+[program:walma]
+directory=/opt/walma
+command=npm start
 autostart=true
 autorestart=true
-stderr_logfile=/var/log/etherpad.err.log
-stdout_logfile=/var/log/etherpad.out.log
+stderr_logfile=/var/log/walma.err.log
+stdout_logfile=/var/log/walma.out.log
 EOT
 
 ## Run
